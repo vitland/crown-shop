d529d9913e1e48daea52f678d54ff4b5550cc611
Victor <vitland@gmail.com>
2022-04-29 13:12:40 +0300

    init

---
+++ src/components/menu/categories-menu.component.jsx
@@ -0,0 +1,17 @@
+import { CategoryItem } from '../category-item/category-item.component'
+import '../menu/categories-menu.styles.scss'
+
+
+
+export const CategoriesMenu = ({categories}) => {
+  
+
+  return (
+    <div className="menu-container">
+    {categories.map((category) => (
+      <CategoryItem key={category.id} category={category}/>
+    ))}
+  </div>
+    
+  )
+}
